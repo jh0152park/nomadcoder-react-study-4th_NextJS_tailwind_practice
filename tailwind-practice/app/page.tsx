3df8515e6256fc7c5761a7c5d31ff21be187d69d
jh0152park <jh0152park@naver.com>
2024-04-02 18:06:34 +0900

    added dark mode

--- tailwind-practice/app/page.tsx
+++ tailwind-practice/app/page.tsx
@@ -1,13 +1,15 @@
 export default function Home() {
   return (
-    <main className="flex h-screen items-center justify-center bg-gray-300 p-5 text-black ">
-      <div className=" w-full rounded-2xl bg-white p-5 shadow-lg">
+    <main className="flex h-screen items-center justify-center bg-gray-300 p-5 text-black dark:bg-[#121212] ">
+      <div className=" w-full max-w-screen-sm rounded-2xl bg-white p-5 shadow-lg dark:bg-gray-600 ">
         <div className=" flex items-center justify-between">
           <div className=" flex flex-col">
-            <span className=" -mb-1 font-semibold text-gray-600">
+            <span className=" -mb-1 font-semibold text-gray-600 dark:text-gray-400">
               In transit
             </span>
-            <span className=" text-4xl font-semibold">Coolblue</span>
+            <span className=" text-4xl font-semibold dark:text-white">
+              Coolblue
+            </span>
           </div>
           <div className=" size-12 rounded-full bg-orange-400" />
         </div>
@@ -21,11 +23,11 @@ export default function Home() {
           <div className=" absolute h-2 w-full rounded-full bg-gray-200" />
           <div className=" absolute h-2 w-2/3 rounded-full bg-green-400" />
         </div>
-        <div className=" mt-5 flex items-center justify-between text-gray-600">
+        <div className=" mt-5 flex items-center justify-between text-gray-600 dark:text-gray-400">
           <span>Expected</span>
           <span>Sorting center</span>
           <span>In transit</span>
-          <span className=" text-gray-400">Delivered</span>
+          <span className=" text-gray-400 dark:text-gray-500">Delivered</span>
         </div>
       </div>
     </main>
