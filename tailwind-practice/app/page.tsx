911f400f470e641ed0813d3607137aa86773bf2e
jh0152park <jh0152park@naver.com>
2024-04-02 23:25:49 +0900

    animate ping

--- tailwind-practice/app/page.tsx
+++ tailwind-practice/app/page.tsx
@@ -1,34 +1,19 @@
 export default function Home() {
   return (
     <main className="flex h-screen items-center justify-center bg-gray-300 p-5 text-black dark:bg-[#121212] ">
-      <div className=" w-full max-w-screen-sm rounded-2xl bg-white p-5 shadow-lg dark:bg-gray-600 ">
-        <div className=" flex items-center justify-between">
-          <div className=" flex flex-col">
-            <span className=" -mb-1 font-semibold text-gray-600 dark:text-gray-400">
-              In transit
-            </span>
-            <span className=" text-4xl font-semibold dark:text-white">
-              Coolblue
-            </span>
+      <div className=" flex w-full max-w-screen-sm flex-col gap-5 rounded-2xl bg-white p-5 shadow-lg dark:bg-gray-600 dark:text-white ">
+        {["Nico", "Me", "You", "Yourself"].map((person, index) => (
+          <div
+            key={index}
+            className="flex items-center gap-5 rounded-xl p-2.5 odd:bg-gray-500 even:bg-slate-100"
+          >
+            <div className="size-10 rounded-full bg-blue-400" />
+            <span className=" text-lg font-medium">{person}</span>
+            <div className=" flex size-6 animate-ping items-center justify-center rounded-full bg-red-500">
+              <span>{index}</span>
+            </div>
           </div>
-          <div className=" size-12 rounded-full bg-orange-400" />
-        </div>
-        <div className=" my-2 flex items-center gap-2">
-          <span className=" rounded-full bg-green-400 px-2.5 py-1.5 text-xs font-medium uppercase text-white">
-            Today
-          </span>
-          <span className=" font-semibold">9:30-10:30</span>
-        </div>
-        <div className=" relative">
-          <div className=" absolute h-2 w-full rounded-full bg-gray-200" />
-          <div className=" absolute h-2 w-2/3 rounded-full bg-green-400" />
-        </div>
-        <div className=" mt-5 flex items-center justify-between text-gray-600 dark:text-gray-400">
-          <span>Expected</span>
-          <span>Sorting center</span>
-          <span>In transit</span>
-          <span className=" text-gray-400 dark:text-gray-500">Delivered</span>
-        </div>
+        ))}
       </div>
     </main>
   );
